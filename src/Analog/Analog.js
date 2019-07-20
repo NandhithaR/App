1d3cf870657419e1e5b0cb3bb7808f2bb0e0f053
NandhithaR <nandhitha9@gmail.com>
2019-07-20 11:04:36 +0530

    changes to slider illustration

---
+++ src/Analog/Analog.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Analog(){
+    return(
+        <h1>Analog Content</h1>
+    )
+}
+export default Analog; 
---
+++ src/Tools/Tools.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Tools(){
+    return(
+        <h1>Tools Content</h1>
+    )
+}
+export default Tools; 

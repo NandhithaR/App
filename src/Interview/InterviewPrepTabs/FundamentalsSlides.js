6c218aed10bea82205ea2f20e27486d978dad6c4
NandhithaR <nandhitha9@gmail.com>
2019-07-29 17:02:15 +0300

    added styles

--- src/Interview/InterviewPrepTabs/FundamentalsSlides.js
+++ src/Interview/InterviewPrepTabs/FundamentalsSlides.js
@@ -4,6 +4,29 @@ import YouTube from 'react-youtube';
 import {Button} from "react-bootstrap";
 import './style.css'
 
+const topiccontent1=[
+  <p>content of topic 1</p>
+];
+const topiccontent2=[
+  <p>content of topic 2</p>
+];
+const topiccontent3=[
+  <p>content of topic 3</p>
+];
+const topiccontent4=[
+  <p>content of topic 4</p>
+];
+const topiccontent5=[
+  <p>content of topic 5</p>
+];
+const VideoContent = [
+  topiccontent1,
+  topiccontent2,
+  topiccontent3,
+  topiccontent4,
+  topiccontent5
+];
+
 const opts = {
     height: '390',
     width: '640',
@@ -13,11 +36,11 @@ const opts = {
   };
 
   const listItems = [
-    {videoId:"CeD2L6KbtVM", topic_prev:"Last", topic_next:"Topic 2", topic_curr:"Topic 1"},
-    {videoId:"BqP6sVYlrr0", topic_prev:"Topic 1", topic_next:"Topic 3", topic_curr:"Topic 2"},
-    {videoId:"sUutDs7FFeA", topic_prev:"Topic 2", topic_next:"Topic 4", topic_curr:"Topic 3" },
-    {videoId:"XCiLHOZsQl8", topic_prev:"Topic 3", topic_next:"Topic 5", topic_curr:"Topic 4" },
-    {videoId:"FwJalVfvn50", topic_prev:"Topic 4", topic_curr:"Topic 5", topic_next:"First"}
+    {videoId:"CeD2L6KbtVM", topic_prev:"Last", topic_next:"Topic 2", topic_curr:"Topic 1",content:VideoContent[0]},
+    {videoId:"BqP6sVYlrr0", topic_prev:"Topic 1", topic_next:"Topic 3", topic_curr:"Topic 2",content:VideoContent[1]},
+    {videoId:"sUutDs7FFeA", topic_prev:"Topic 2", topic_next:"Topic 4", topic_curr:"Topic 3",content:VideoContent[2] },
+    {videoId:"XCiLHOZsQl8", topic_prev:"Topic 3", topic_next:"Topic 5", topic_curr:"Topic 4",content:VideoContent[3] },
+    {videoId:"FwJalVfvn50", topic_prev:"Topic 4", topic_curr:"Topic 5", topic_next:"First",content:VideoContent[4]}
   ];
 
     
@@ -88,12 +111,13 @@ class FundamentalsSlides extends React.Component {
   render() {
     return (
         <div>
+        <h1>{this.state.slideshow.topic_curr}</h1>
         <SlideItem videoId={this.state.slideshow.videoId} />
         <Indicators 
           changeSlide={this.goTo} // function
           currentSlide={this.state.slideIndex}
           />
-        <div>Conent for the video</div>
+        <div>{this.state.slideshow.content}</div>
         <div className="wrap-control">
           <Button className="btn btn-prev btn-success" value="Prev" onClick={() => this.goTo('prev')}>{this.state.slideshow.topic_prev}</Button>
           <Button className="btn btn-next btn-success" value="Next" onClick={() => this.goTo('next')}>{this.state.slideshow.topic_next}</Button>          

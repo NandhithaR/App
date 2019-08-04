c566473e08362e17e25842dcffa7b1ea4ce57d3a
NandhithaR <nandhitha9@gmail.com>
2019-08-04 14:42:26 +0300

    changed file name

--- src/Interview/InterviewPrepTabs/CompanyQuestions.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-function CompanyQuestion(){
-    return(
-        <h1>CompanyQuestion Content</h1>
-    )
-}
-export default CompanyQuestion; 

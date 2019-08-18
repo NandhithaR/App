e0e02094edea2b43e1ae9e9d5be161e5bd20277f
NandhithaR <nandhitha9@gmail.com>
2019-08-18 19:50:41 -0400

    modified

---
+++ src/Interview/InterviewPrepTabs/InterviewQuestionsData/interviewques.js
@@ -0,0 +1,93 @@
+import sunrise from './circuitimages/sunrise.jpeg'
+import circuit1 from './circuitimages/circuit1.GIF'
+import circuit2 from './circuitimages/circuit2.jpg'
+import circuit3 from './circuitimages/circuit3.gif'
+import circuit4 from './circuitimages/circuit4.gif'
+import circuit6 from './circuitimages/circuit6.png'
+import circuit7 from './circuitimages/circuit7.png'
+import circuit9 from './circuitimages/circuit9.gif'
+import circuit11 from './circuitimages/circuit11.gif'
+import circuit12 from './circuitimages/circuit12.gif'
+import circuit13 from './circuitimages/circuit13.gif'
+import circuit14 from './circuitimages/circuit14.png'
+import circuit16 from './circuitimages/circuit16.gif'
+import circuit17 from './circuitimages/circuit17.png'
+import circuit18 from './circuitimages/circuit18.gif'
+import circuit19 from './circuitimages/circuit19.gif'
+import circuit20 from './circuitimages/circuit20.gif'
+import circuit21 from './circuitimages/circuit21.gif'
+import circuit22 from './circuitimages/circuit22.gif'
+import circuit23 from './circuitimages/circuit23.gif'
+import circuit25 from './circuitimages/circuit25.gif'
+import circuit26 from './circuitimages/circuit26.jpg'
+import circuit27 from './circuitimages/circuit27.jpg'
+import circuit28 from './circuitimages/circuit28.png'
+import circuit30 from './circuitimages/circuit30.gif'
+import circuit31 from './circuitimages/circuit31.gif'
+import circuit32 from './circuitimages/circuit32.gif'
+import circuit33 from './circuitimages/circuit33.gif'
+const questions={
+  1:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch?",
+    Image:circuit1,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  2:{
+    Index: 2,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit2,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  3:{
+    Index: 3,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit3,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  4:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit4,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  5:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit6,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  6:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit7,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  7:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit9,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  8:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit11,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  9:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit12,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  10:{
+    Index: 1,
+    Question: "Two capacitors are connected in parallel through a switch. C1= 1uF, C2= 0.25uF. Initially the switch is open, C1 is charged to 10V. What happens if we close the switch? No losses in wires and capacitors. ",
+    Image:circuit13,
+    Answer: "The equation for charge is Q=CU After closing the switch the charge will be distributed between the two caps. Since there are no losses in the circuit the amount of charge remains the same: U1C1 + U2C2 = U3(C1+C2) Both caps will share the same voltage potential U3, U3 =  (U1C1+U2C2)/(C1+C2) = (10*1 + 0*0.25)/1+0.25 = 8 U3= 8V "
+  },
+  
+}
+
+export default questions

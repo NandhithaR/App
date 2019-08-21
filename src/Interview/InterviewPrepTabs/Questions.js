bfd11334e33bab153280116d701be004d8a19fd9
NandhithaR <nandhitha9@gmail.com>
2019-08-21 17:07:56 -0400

    added images

--- src/Interview/InterviewPrepTabs/Questions.js
+++ src/Interview/InterviewPrepTabs/Questions.js
@@ -5,7 +5,7 @@ import './questions.css'
 //import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import * as Scroll from 'react-scroll';
 import { Link,DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
-import { Collapse, Button} from 'reactstrap';
+import {Button} from 'reactstrap';
 
 class ToggleQuestion extends React.Component {
   constructor (props) {
@@ -38,8 +38,6 @@ class Questions extends React.Component {
   constructor(props) {
     super(props);
     this.scrollToTop = this.scrollToTop.bind(this);
-    // this.toggle = this.toggle.bind(this);
-    // this.state = { collapse: false };
     this.state = {
       // start the page with all questions closed
     	selectedQuestion: -1
@@ -47,10 +45,6 @@ class Questions extends React.Component {
     this.openQuestion = this.openQuestion.bind(this);
     
   }
-
-  // toggle() {
-  //   this.setState(state => ({ collapse: !state.collapse }));
-  // }
   openQuestion(index) {
     // when a question is opened, compare what was clicked and if we got a match, change state to show the desired question.
   	this.setState({
@@ -113,58 +107,32 @@ class Questions extends React.Component {
     const { details, expanded,selectedQuestion } = this.state;
     return (
       <div>
-          {/* <nav className="navbar navbar-default navbar-fixed-top">
-            <div className="container-fluid">
-              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> */}
-                <ul id="links">
-                  <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Topic1</Link></li>
-                  <li><Link activeClass="active" className="test2" to="test2" spy={true} smooth={true} duration={500} >Topic2</Link></li>
-                  {/* <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Show/Hide Answer</Button> */}
+        <ul id="links">
+          <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Topic1</Link></li>
+          <li><Link activeClass="active" className="test2" to="test2" spy={true} smooth={true} duration={500} >Topic2</Link></li>
+        </ul>
 
-                </ul>
-              {/* </div>
-            </div>
-          </nav> */}
-
-          <Element name="test1" className="element" >
+        <Element name="test1" className="element" >
           <div id="topic1">
             <h2>Topic1</h2>
-            {questions.length && questions.map((item, index) => (
-            <div key={`item-${index}`} className={`item ${this.state.selectedQuestion === index ? 'open' : ''}`}>
-                <p className='question' >
-                  {item.Question}
-                  <img src={item.Image} />
-                </p>
-                <Button color="primary" onClick={() => this.openQuestion(index)} style={{ marginBottom: '1rem' }}>Answer</Button>
-                <p className='answer'>
-                  {item.Answer}
-                  <img src={item.AnswerImage} />
-                </p>
-            </div>
-          ))}
-          </div>
-        </Element>
-
-          {/* <Element name="test1" className="element" >
-          <div id="topic1">
-            <h2>Topic1</h2>
-              {Object.keys(questions).map(key => (
-                <div>
-                  {questions[key].type}
-                  <p id="ques">{questions[key].Index}.  {questions[key].Question}</p>
-                  <img src={questions[key].Image} key={key} />
-                 <div>
-                 <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Answer</Button>
-                    <Collapse isOpen={this.state.collapse}>
-                          <p id="ans">{questions[key].Answer}</p>
-                          <img src={questions[key].AnswerImage} />
-                    </Collapse>
-                 </div>
-                
-                </div>
+              {questions.length && questions.map((item, index) => (
+              <div key={`item-${index}`} className={`item ${this.state.selectedQuestion === index ? 'open' : ''}`}>
+                  <p className='question' >
+                    {item.Index}.{item.Question}
+                    <img src={item.Image} />
+                  </p>
+                  <Button color="success" onClick={() => this.openQuestion(index)} style={{ marginBottom: '1rem' }}>Answer</Button>
+                  <p id="ans" className='answer'>
+                    {item.Answer}
+                    <img src={item.AnswerImage} />
+                    <img src={item.AnswerImage1} />
+                    <img src={item.AnswerImage2} />
+                    <img src={item.AnswerImage3} />
+                  </p>
+              </div>
               ))}
           </div>
-        </Element> */}
+        </Element>
 
           {/* <Element name="test2" className="element">
           <div id="topic2">

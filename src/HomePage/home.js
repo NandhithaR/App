df9c653bf3643ed6ade71fbf94f40a6c42471ee9
NandhithaR <nandhitha9@gmail.com>
2019-08-21 17:07:11 -0400

    commented some code to be modified later

--- src/HomePage/home.js
+++ src/HomePage/home.js
@@ -11,19 +11,19 @@ import 'react-dropdown/style.css'
 
 function Home(){
     return(
-        <div>
+        <div id="homeid">
             <Router>
                 <div className="topnav">
                     <Link to="/" className="active" href="#home">Home</Link>
-                    <Link to="/analog">Analog</Link>
-                    <Link to="/digital">Digital</Link>
+                    {/* <Link to="/analog">Analog</Link> */}
+                    {/* <Link to="/digital">Digital</Link> */}
                     <Link to="/interview">Interview Prep</Link>
-                    <Link to="/tools">Tools</Link>
+                    {/* <Link to="/tools">Tools</Link> */}
                 </div>
-                <Route path="/digital" component={Digital} />
+                {/* <Route path="/digital" component={Digital} /> */}
                 <Route path="/interview" exact strict component={Interview} />
-                <Route path="/tools" exact strict component={Tools} />
-                <Route path="/analog" exact strict component={Analog} />
+                {/* <Route path="/tools" exact strict component={Tools} />
+                <Route path="/analog" exact strict component={Analog} /> */}
                 <Route path="/" exact strict component={HomeScreen} />
             </Router>
         </div>

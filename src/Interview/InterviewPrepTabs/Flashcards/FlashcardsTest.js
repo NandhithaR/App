ee7523d674a7d6bf4471e10b4a4cde3a28192705
NandhithaR <nandhitha9@gmail.com>
2019-09-04 23:03:08 -0400

    added fillintheblanks

--- src/Interview/InterviewPrepTabs/Flashcards/FlashcardsTest.js
+++ src/Interview/InterviewPrepTabs/Flashcards/FlashcardsTest.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {data,fillblanks} from './Equations/FlashcardContent'
+import MCQ from './FlashcardsQuestions/MCQ'
+import FillintheBlanks from './FlashcardsQuestions/FillintheBlanks'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function FlashcardsTest(){
-    return(<h1>Test</h1>)
+    return(<div>
+            <Router>
+                    <div>
+                    <Link to="/flashcards/test/mcq" id="test">MCQ</Link>
+                    <Link to="/flashcards/test/qa" id="learn">Fill in the Blanks</Link>
+                    </div>
+                    <div>
+                    <Route path="/flashcards/test/mcq" exact strict component={MCQ}/>
+                    <Route path="/flashcards/test/qa" exact strict component={FillintheBlanks}/>
+                    </div>
+            </Router>
+           </div>
+        )
 }
 
 export default FlashcardsTest
